9680e50bc7b3e8ab2b9e9257ed2e5e3838466e65
= <=>
2024-08-17 00:35:01 +0900

    Connected DB in Docker and configured JPA

---
+++ src/main/java/com/solfamily/istory/db/UserRepository.java
@@ -0,0 +1,10 @@
+package com.solfamily.istory.db;
+
+import com.solfamily.istory.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 아래는 제네릭 안에 들어가는 값에 대한 설명
+// 데이터베이스에서 UserEntity와 관련된 CRUD(Create, Read, Update, Delete) 작업을 처리할 수 있게 설정
+// 기본 키의 타입이 Long임을 JpaRepository에 알려주는 역할
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
