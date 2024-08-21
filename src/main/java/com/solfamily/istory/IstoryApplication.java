77ee1fb919a6b0f42c36f76bf69a594f041031d4
이용태 <ioosulsa@gmail.com>
2024-08-21 21:25:45 +0900

    mod main

--- src/main/java/com/solfamily/istory/IstoryApplication.java
+++ src/main/java/com/solfamily/istory/IstoryApplication.java
@@ -2,8 +2,10 @@ package com.solfamily.istory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.solfamily.istory.global")
 public class IstoryApplication {
 
 	public static void main(String[] args) {
