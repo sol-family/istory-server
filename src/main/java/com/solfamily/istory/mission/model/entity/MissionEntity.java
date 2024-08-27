41745f2f7a6efe5d98414a37843b7bb9454b9467
SeongMun Woo <tjdans7665@gmail.com>
2024-08-27 09:02:01 +0900

    revert mission

---
+++ src/main/java/com/solfamily/istory/mission/model/entity/MissionEntity.java
@@ -0,0 +1,23 @@
+package com.solfamily.istory.mission.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="istory_mission")
+@Entity
+public class MissionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="mission_no",nullable = false)
+    private Long missionNo;
+
+    @Column(name="contents",nullable = false)
+    private String contents;
+}
