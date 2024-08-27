bc99972864dde93b0dc287064d8b651225aa77ce
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-08-28 00:18:14 +0900

    1. Refactoring API 2. set Filtering - Filtering settings for API requests after login before the request is mapped to the controller

---
+++ src/main/java/com/solfamily/istory/global/configure/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.solfamily.istory.global.configure;
+
+import com.solfamily.istory.global.service.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter() {
+        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(jwtAuthenticationFilter);
+        registrationBean.addUrlPatterns("/api/v1/*"); // 필터가 적용될 URL 패턴 설정
+        return registrationBean;
+    }
+}
