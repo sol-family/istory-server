cf0c15757f642eefbca98baf8396c2984cdc72bc
Victra15 <Victra15@github.com>
2024-08-30 13:10:46 +0900

    mod RedisConfigure

--- src/main/java/com/solfamily/istory/global/configure/RedisConfigure.java
+++ src/main/java/com/solfamily/istory/global/configure/RedisConfigure.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -20,7 +21,10 @@ public class RedisConfigure {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory () {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
